81194bdf85f41c4470e18f157d7266373b8f5914
caio-villardo <cv.jardelino@gmail.com>
2025-07-14 10:53:17 -0300

    orderby add to list roles

--- internal/infra/grpc_server/controllers/roles_controller/get.go
+++ internal/infra/grpc_server/controllers/roles_controller/get.go
@@ -2,30 +2,39 @@ package roles_controller
 
 import (
 	"context"
-	"fmt"
 	"permissions-service/internal/adapters/grpc_convertions"
 	"permissions-service/internal/app/ent"
 	"permissions-service/internal/app/ent/role"
 
 	"github.com/dev-star-company/protos-go/permissions_service/generated_protos/roles_proto"
+	"github.com/google/uuid"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (c *controller) Get(ctx context.Context, in *roles_proto.GetRequest) (*roles_proto.GetResponse, error) {
-	role, err := c.Db.Role.Query().
+
+	if in.Id == 0 {
+		return nil, status.Error(codes.InvalidArgument, "role ID is required")
+	}
+
+	if _, err := uuid.Parse(in.RequesterUuid); err != nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid requester UUID")
+	}
+
+	roleEntity, err := c.Db.Role.
+		Query().
 		Where(role.IDEQ(int(in.Id))).
 		Only(ctx)
 	if err != nil {
 		if ent.IsNotFound(err) {
 			return nil, status.Error(codes.NotFound, "role not found")
 		}
-
-		return nil, fmt.Errorf("querying role: %w", err)
+		return nil, status.Errorf(codes.Internal, "error retrieving role: %v", err)
 	}
 
 	return &roles_proto.GetResponse{
-		Role: grpc_convertions.RoleToProto(role),
+		Role: grpc_convertions.RoleToProto(roleEntity),
 	}, nil
 }

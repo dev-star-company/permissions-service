54058c42159f803d7378c3f8e23790934e18b2c9
caio-villardo <cv.jardelino@gmail.com>
2025-07-17 15:15:39 -0300

    otimization

--- internal/infra/grpc_server/controllers/auth_users_controller/update.go
+++ internal/infra/grpc_server/controllers/auth_users_controller/update.go
@@ -3,11 +3,9 @@ package auth_users_controller
 import (
 	"context"
 	"permissions-service/internal/adapters/grpc_convertions"
-	"permissions-service/internal/app/ent"
 	userSchema "permissions-service/internal/app/ent/user"
 	"permissions-service/internal/infra/grpc_server/controllers"
 	"permissions-service/internal/pkg/utils"
-	"sync"
 
 	"github.com/dev-star-company/protos-go/permissions_service/generated_protos/auth_users_proto"
 
@@ -24,41 +22,18 @@ func (c *controller) Update(ctx context.Context, in *auth_users_proto.UpdateRequ
 		return nil, errs.StartTransactionError(err)
 	}
 
-	var wg sync.WaitGroup
-	var requester *ent.User
-	var target *ent.User
-
-	errCh := make(chan error, 2)
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		requester, err = controllers.GetUserFromUuid(tx, ctx, in.RequesterUuid)
-		if err != nil {
-			errCh <- err
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		target, err = controllers.GetUserFromUuid(tx, ctx, *in.Uuid)
-		if err != nil {
-			errCh <- err
-		}
-	}()
-
-	done := make(chan struct{})
-	go func() {
-		wg.Wait()
-		close(done)
-	}()
-
-	select {
-	case err := <-errCh:
+	requester, err := controllers.GetUserFromUuid(tx, ctx, in.RequesterUuid)
+	if err != nil {
 		return nil, err
-	case <-done:
-		// No errors, continue
+	}
+
+	if in.Uuid == nil {
+		return nil, errs.RequesterIDRequired()
+	}
+
+	target, err := controllers.GetUserFromUuid(tx, ctx, *in.Uuid)
+	if err != nil {
+		return nil, err
 	}
 
 	userQ := tx.User.UpdateOneID(int(target.ID)).SetUpdatedBy(requester.ID)
@@ -74,28 +49,6 @@ func (c *controller) Update(ctx context.Context, in *auth_users_proto.UpdateRequ
 	if err != nil {
 		return nil, utils.Rollback(tx, err)
 	}
-
-	// if in.Password != nil && in.ConfirmPassword != nil && *in.Password == *in.ConfirmPassword {
-	// 	p, err := tx.Password.Create().
-	// 		SetPassword(*in.Password).
-	// 		SetCreatedBy(requester.ID).
-	// 		SetUpdatedBy(requester.ID).
-	// 		SetUserID(user.ID).
-	// 		Save(ctx)
-	// 	if err != nil {
-	// 		return nil, utils.Rollback(tx, err)
-	// 	}
-
-	// 	oldPassword := tx.Password.Query().
-	// 		Where(password.IDNotIn(p.ID), password.HasUserWith(userSchema.IDEQ(user.ID))).
-	// 		Order(ent.Desc(password.FieldID)).
-	// 		FirstX(ctx)
-
-	// 	err = tx.Password.DeleteOneID(oldPassword.ID).Exec(ctx)
-	// 	if err != nil {
-	// 		return nil, utils.Rollback(tx, err)
-	// 	}
-	// }
 
 	if err := tx.Commit(); err != nil {
 		return nil, utils.Rollback(tx, err)
